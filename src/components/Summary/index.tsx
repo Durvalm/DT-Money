dc95cad0d437819dfa0f2459a05c08e96e3e64aa
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2023-12-26 19:13:53 -0500

    table of deposits/withdrawals

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,10 +6,10 @@ export function Summary() {
         <SummaryContainer>
             <SummaryCard>
                 <header>
-                    <span>Total</span>
+                    <span>Deposits</span>
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>$ 17.400,00</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -17,7 +17,7 @@ export function Summary() {
                     <span>Withdrawals</span>
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>$ 17.400,00</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -25,7 +25,7 @@ export function Summary() {
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>$ 17.400,00</strong>
             </SummaryCard>
 
 

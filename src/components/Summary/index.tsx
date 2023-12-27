2d91931b04b3c72dbe1b924f0ff5b149cd37bc25
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2023-12-27 12:40:48 -0500

    reducer to create summary of deposits | withdrawals

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,6 +6,21 @@ import { useContext } from "react";
 export function Summary() {
     const {transactions} = useContext(TransactionsContext)
 
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'income'){
+           acc.income += transaction.price
+           acc.total += transaction.price
+        } else {
+           acc.outcome += transaction.price
+           acc.total -= transaction.price
+        }
+        return acc
+    }, {
+        'income': 0,
+        'outcome': 0,
+        'total': 0
+    })
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -13,7 +28,7 @@ export function Summary() {
                     <span>Deposits</span>
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
-                <strong>$ 17,442.53</strong>
+                <strong>$ {summary.income}</strong>
             </SummaryCard>
 
             <SummaryCard>
@@ -21,7 +36,7 @@ export function Summary() {
                     <span>Withdrawals</span>
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
-                <strong>$ 2,462.62</strong>
+                <strong>$ {summary.outcome}</strong>
             </SummaryCard>
 
             <SummaryCard variant="green">
@@ -29,7 +44,7 @@ export function Summary() {
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
-                <strong>$ 405,050.72</strong>
+                <strong>$ {summary.total}</strong>
             </SummaryCard>
 
 

1ecf8c6076e4d1af4e32462cc058d25b8a610378
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2023-12-27 14:18:48 -0500

    React Hook Form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as Dialog from '@radix-ui/react-dialog';
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
@@ -10,7 +10,7 @@ const newTransactionFormSchema = z.object({
     description: z.string(),
     price: z.number(),
     category: z.string(),
-    // type: z.enum(['income', 'outcome']),
+    type: z.enum(['income', 'outcome']),
   });
   
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
@@ -18,10 +18,11 @@ type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 export function NewTransactionModal(){
 
     const {
+        control,
         register,
         handleSubmit,
         formState: { isSubmitting }
-      } = useForm<NewTransactionFormInputs>({
+    } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema),
       })
 
@@ -51,16 +52,22 @@ export function NewTransactionModal(){
                 {...register('category')}
                 />
 
-                <TransactionType>
-                    <TransactionTypeButton variant='income' value='income'>
-                        <ArrowCircleUp size={24} />
-                        Deposit
-                    </TransactionTypeButton >
-                    <TransactionTypeButton variant='outcome' value='outcome'>
-                        <ArrowCircleDown size={24} />
-                        Withdraw
-                    </TransactionTypeButton>
-                </TransactionType>
+                <Controller control={control} name="type"
+                  render={({ field }) => {
+                    return (
+                    <TransactionType onValueChange={field.onChange} value={field.value}>
+                        <TransactionTypeButton variant='income' value='income'>
+                            <ArrowCircleUp size={24} />
+                            Deposit
+                        </TransactionTypeButton >
+                        <TransactionTypeButton variant='outcome' value='outcome'>
+                            <ArrowCircleDown size={24} />
+                            Withdraw
+                        </TransactionTypeButton>
+                    </TransactionType>
+                    )
+                }} />
+
 
                 <button type="submit" disabled={isSubmitting}>
                     Submit
